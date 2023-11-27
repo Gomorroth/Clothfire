92c6dd7c73d64212dba2ad075ffa17963c67bb68
Grabacr Scarlet <73260157+GrabacrScarlet@users.noreply.github.com>
2023-11-27 12:20:05 +0900

    Add 'GetControlTargets' method

--- Runtime/Script/ControlTarget.cs
+++ Runtime/Script/ControlTarget.cs
@@ -11,6 +11,17 @@ namespace gomoru.su.clothfire
     {
         internal static readonly List<ControlTarget> SharedList = new List<ControlTarget>();
 
+        public static ReadOnlySpan<ControlTarget> GetControlTargets(GameObject root)
+        {
+            var list = SharedList;
+            list.Clear();
+            foreach(var x in root.GetComponentsInChildren<IControlTargetProvider>())
+            {
+                x.GetControlTargets(list);
+            }
+            return list.AsSpan();
+        }
+
         public Object Parent;
         public string Path;
 
