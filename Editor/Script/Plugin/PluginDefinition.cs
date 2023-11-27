0f022e292c7bab13aa52177a352c5cd728982a3a
Ashley Scarlet <70315656+AshleyScarlet@users.noreply.github.com>
2023-11-27 20:49:19 +0900

    Add PluginDefinition

---
+++ Editor/Script/Plugin/PluginDefinition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using nadena.dev.ndmf;
+
+namespace gomoru.su.clothfire
+{
+    internal sealed class PluginDefinition : Plugin<PluginDefinition>
+    {
+        public override string DisplayName => "Clothfire";
+        public override string QualifiedName => "gomoru.su.clothfire";
+
+        protected override void Configure()
+        {
+
+        }
+    }
+}
