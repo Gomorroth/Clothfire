883c982998693a9fc0c3b758254c0103b6b9da44
Ashley Scarlet <70315656+AshleyScarlet@users.noreply.github.com>
2023-12-02 18:31:57 +0900

    特に意味のないエディタ

---
+++ Editor/Script/Inspector/ConfigurationEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using nadena.dev.modular_avatar.core;
+
+namespace gomoru.su.clothfire
+{
+    [CustomEditor(typeof(Configuration))]
+    internal sealed class ConfigurationEditor : Editor
+    {
+        internal void OnEnable()
+        {
+        }
+
+        public override void OnInspectorGUI()
+        {
+        }
+    }
+}
