d32d4f5d5dad136b08c48a4d6ba949a8be26dad6
Grabacr Scarlet <73260157+GrabacrScarlet@users.noreply.github.com>
2023-12-04 13:23:11 +0900

    マテリアル操作追加

--- Runtime/Script/AdditionalControl.cs
+++ Runtime/Script/AdditionalControl.cs
@@ -9,13 +9,15 @@ namespace gomoru.su.clothfire
         public enum ControlType
         {
             Blendshape,
-            AnimationClip
+            AnimationClip,
+            Material
         }
 
         public ControlType Type;
         public bool IsAbsolute = false;
         public BlendshapeControl Blendshape;
         public AnimationControl Animation;
+        public MaterialControl Material;
 
         [NonSerialized]
         public GameObject Root;
---
+++ Runtime/Script/MaterialControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace gomoru.su.clothfire
+{
+    [Serializable]
+    public sealed class MaterialControl
+    {
+        public Renderer Target;
+        public Material OFF;
+        public Material ON;
+        public bool IsChangeOFF = true;
+        public bool IsChangeON = true;
+    }
+}
