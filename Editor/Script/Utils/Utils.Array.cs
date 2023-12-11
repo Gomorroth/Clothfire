162cad55347af017c8fb9ded93886f829eac5578
Grabacr Scarlet <73260157+GrabacrScarlet@users.noreply.github.com>
2023-12-11 13:35:34 +0900

    Create Utils.Array.cs

---
+++ Editor/Script/Utils/Utils.Array.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gomoru.su.clothfire
+{
+    partial class Utils
+    {
+        public static T[] Single<T>() => SingleArrayCache<T>.Single;
+
+        private static class SingleArrayCache<T>
+        {
+            public static readonly T[] Single = new T[1];
+        }
+    }
+}
