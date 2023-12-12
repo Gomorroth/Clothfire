34af1d3ca701133b706e8faa1fddc457b2e8a587
Grabacr Scarlet <73260157+GrabacrScarlet@users.noreply.github.com>
2023-12-12 10:34:05 +0900

    Add GroupSelector

---
+++ Editor/Script/GroupSelector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace gomoru.su.clothfire
+{
+    internal sealed class GroupSelector : SelectorWindow<string>
+    {
+        public static void Show(Rect position, GameObject avatarRootObject, Action<string> callback)
+        {
+            if (avatarRootObject == null)
+                return;
+
+            var groups = avatarRootObject.GetComponentsInChildren<IControlGroup>().Select(x => x.GroupName).Distinct().ToTemporaryMemory();
+
+            Show(position, groups, callback, new GUIContent($"Select groups"));
+        }
+
+        protected override GUIContent GetGUIContent(ref string item) => item.ToGUIContent();
+    }
+}
