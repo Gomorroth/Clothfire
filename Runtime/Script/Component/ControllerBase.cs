5b87155b975597701bf7ddb083676661a5a9c694
Grabacr Scarlet <73260157+GrabacrScarlet@users.noreply.github.com>
2023-12-12 11:58:35 +0900

    Add ControllerBase

---
+++ Runtime/Script/Component/ControllerBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace gomoru.su.clothfire
+{
+    public abstract class ControllerBase : ClothfireBaseComponent, IControlGroup, IAdditionalControlProvider
+    {
+        public string MenuName;
+        public string Group;
+
+        string IControlGroup.GroupName => Group;
+
+        GameObject IControlGroup.GroupMaster => gameObject;
+
+        GroupType IControlGroup.GroupType => GroupType.Control;
+
+        GameObject IAdditionalControlProvider.GameObject => gameObject;
+
+        void IAdditionalControlProvider.GetAdditionalControls(AdditionalControlContainer destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
