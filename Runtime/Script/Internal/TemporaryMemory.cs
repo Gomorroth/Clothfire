8d057a6405fddbe0c521dd00b48bdaa742096f6f
Grabacr Scarlet <73260157+GrabacrScarlet@users.noreply.github.com>
2023-12-12 10:21:43 +0900

    Add TemporaryMemory

---
+++ Runtime/Script/Internal/TemporaryMemory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gomoru.su.clothfire
+{
+    internal readonly struct TemporaryMemory<T> : IDisposable
+    {
+        private readonly T[] _buffer;
+        private readonly int _size;
+
+        public Memory<T> Memory => _buffer.AsMemory(0, _size);
+        public Span<T> Span => _buffer.AsSpan(0, _size);
+
+        public bool IsEmpty => _size == 0;
+
+        private TemporaryMemory(int size)
+        {
+            _buffer = ArrayPool<T>.Shared.Rent(size);
+            _size = size;
+        }
+
+        public static TemporaryMemory<T> Allocate(int size) => new TemporaryMemory<T>(size);
+
+        public void Dispose()
+        {
+            if (_buffer != null)
+                ArrayPool<T>.Shared.Return(_buffer);
+        }
+    }
+}
